22efa92d3f84d71830c44f0ffd4246e0a2a849e5
Noam Mirjani <Noamirjani@gmail.com>
2023-04-23 00:57:22 +0200

    change the state controll of the game from inGame inWin inHighscore to gameState the controlles with stings

--- react-client/src/components/MenuButtons.js
+++ react-client/src/components/MenuButtons.js
@@ -6,13 +6,10 @@ import {useState} from "react";
  * MenuButtons component displays the menu buttons for the game. It receives functions to start a new game, show the rules,
  * and set the game state variables for the current game.
  *
- * @param {function} setInGame - The function to set the "in game" state variable for the current game.
- * @param {function} setInWin - The function to set the "in win" state variable for the current game.
- * @param {function} setInHighScore - The function to set the "in high score" state variable for the current game.
  * @param {function} initFunc - The function to initialize the game state variables for a new game.
  * @returns a JSX element containing the menu buttons.
  */
-function MenuButtons({setInGame, setInWin, setInHighScore, initFunc}) {
+function MenuButtons({initFunc}) {
 
     const [rulesModal, setRulesModal] = useState(false);
 
@@ -20,9 +17,6 @@ function MenuButtons({setInGame, setInWin, setInHighScore, initFunc}) {
      * Starts a new game by calling the provided initFunc and setting the "in game" and "in win" state variables.
      */
     function startNewGame(){
-        setInGame(true);
-        setInWin(false);
-        setInHighScore(false);
         initFunc();
     }
 
